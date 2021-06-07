722499ad6d4830a18b2b79456891dd9726a61254
Luis J. Montes <luisjavm3@gmail.com>
2021-06-07 15:09:10 -0500

    code updateUserController

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,19 @@
+import ErrorResponse from '../utils/ErrorResponse.js';
+import { updateUser } from '../services/userService.js';
+
 export const updateUserController = async (req, res, next) => {
+  // User logged in the system ↓↓↓
   const user = req.user;
+  // User's id to update ↓↓↓
   const { userId } = req.params;
   const newData = req.body;
+  let updatedUser;
 
   try {
-  } catch (error) {}
+    updatedUser = await updateUser(user, newData, userId);
+  } catch (error) {
+    return next(error);
+  }
 
-  res.send(`It works!!!... ${userId}`);
+  res.status(200).json({ success: true, user: updatedUser });
 };
--- services/userService.js
+++ services/userService.js
@@ -1,3 +1,59 @@
-import User from '../model/User.js';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 
-export const updateUser = (req, res, next) => {};
+import User from '../model/User.js';
+import ErrorResponse from '../utils/ErrorResponse.js';
+import { ADMIN, SUPER } from '../config/roles.js';
+const { Types } = mongoose;
+
+/**
+ * This method only changes the role or the password of the user.
+ */
+export const updateUser = async (LoggedUser, newData, userId) => {
+  let user;
+
+  if (!Types.ObjectId.isValid(userId)) {
+    throw new ErrorResponse(`${userID} is not a valid Id.`, 400);
+  }
+
+  if (!newData.role && !newData.password) {
+    throw new ErrorResponse('Insert new data to update.');
+  }
+
+  try {
+    user = await User.findById(userId);
+  } catch (error) {
+    throw new ErrorResponse(`Could not find a user with the Id: ${userId}.`);
+  }
+
+  if ([ADMIN, SUPER].includes(user.role) && LoggedUser.role === ADMIN) {
+    throw new ErrorResponse('Access Forbidden.', 403);
+  }
+
+  // Due the password cannot be decrypted, we need to know whether it has been changed.
+  if (newData.password) {
+    if (newData.password.length < 6 || typeof newData.password !== 'string') {
+      throw new ErrorResponse(
+        'The password must be a string of at least 6 characters.',
+        400
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    newData.password = await bcrypt.hash(newData.password, salt);
+
+    newData = {
+      role: newData?.role || user.role,
+      password: newData.password,
+    };
+  } else {
+    newData = {
+      role: newData.role,
+    };
+  }
+
+  return await User.findByIdAndUpdate(userId, newData, {
+    new: true,
+    runValidators: true,
+  });
+};
